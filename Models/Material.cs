f45283a0aa46b58db6a96a5f7acb7bd94d9b87c1
ricardo <damaceno.ric@gmail.com>
2024-05-01 11:16:00 -0300

    acesso à API

---
+++ Models/Material.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace OrcamentoVinil.Models
+{
+    public class Material
+    {
+        [JsonProperty("id")]
+        public int ID { get; set; }
+
+        [JsonProperty("descricao")]
+        public string Descricao { get; set; }
+
+        [JsonProperty("precoM2")]
+        public decimal PrecoM2 { get; set; }
+
+        [JsonProperty("precoML")]
+        public decimal PrecoML { get; set; }
+    }
+}
---
+++ Util/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace OrcamentoVinil.Util
+{
+    public static class Constants
+    {
+        public const string URL_BASE = "https://apiorcamentovinil.azurewebsites.net";
+    }
+}
