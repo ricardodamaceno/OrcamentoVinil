e4a32e6dfd3805ffecc0715a5051a2fb9accb3e0
ricardo <damaceno.ric@gmail.com>
2024-05-01 13:03:42 -0300

    Alteração 01/05/2024 13:03

---
+++ Models/RequestMaterial.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace OrcamentoVinil.Models
+{
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore, Title = "")]
+    public class RequestMaterial
+    {
+        [JsonProperty("descricao")]
+        public string Descricao { get; set; }
+
+        [JsonProperty("precoM2")]
+        public decimal PrecoM2 { get; set; }
+
+        [JsonProperty("precoML")]
+        public decimal PrecoML { get; set; }
+    }
+}
