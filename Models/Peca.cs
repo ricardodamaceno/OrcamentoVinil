3e7daf23f34e10d92cd0742bf83dc1408ee87ea0
ricardo <damaceno.ric@gmail.com>
2024-05-04 10:10:59 -0300

    Adição de Peças

---
+++ Models/Peca.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace OrcamentoVinil.Models
+{
+    public class Peca
+    {
+        [JsonProperty("id")]
+        public int ID { get; set; }
+
+        [JsonProperty("descricao")]
+        public string Descricao { get; set; }
+    }
+}
---
+++ Models/RequestPeca.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace OrcamentoVinil.Models
+{
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore, Title = "")]
+    public class RequestPeca
+    {
+        [JsonProperty("descricao")]
+        public string Descricao { get; set; }
+    }
+}
